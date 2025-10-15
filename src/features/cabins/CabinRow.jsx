2e2bae58ddb988bea74b134634799e86eccebbb3
DreadTsx <hallowpahe@gmail.com>
2025-10-15 09:21:24 +0100

    installed superbase and react query for remote state management

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +41,45 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
+
+  //?Hook for geeting the queryClient used in the app.jsx
+  const queryClient = useQueryClient();
+
+  //? Hook for mutating a state
+  const { isLoading: isDeleting, mutate } = useMutation({
+    //? function for mutating the state(deleting a cabin using the id)
+    mutationFn: (id) => deleteCabin(id),
+    //? Updating the UI by invalidating the queries
+    onSuccess: () => {
+      alert("Cabin Successfully Deleted");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (err) => alert(err.message),
+  });
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+        Delete
+      </button>
+    </TableRow>
+  );
+}
+
+export default CabinRow;
---
+++ src/services/superbase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://zxxqfglxrxfouumjxjps.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4eHFmZ2x4cnhmb3V1bWp4anBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4MTAyMTksImV4cCI6MjA3NTM4NjIxOX0.YKN5JarqRfgD_CPR819bvxIRh_psUNUglGNCWnVhwbM";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
