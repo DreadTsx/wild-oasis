5d1049e1efefa555d2b41fba25992bc1f8183983
DreadTsx <hallowpahe@gmail.com>
2025-10-16 21:03:30 +0100

    created the toast notification, installed React useForm Library and implementing it

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -6,7 +6,7 @@ const Input = styled.input`
   border-radius: var(--border-radius-sm);
 
   padding: 0.8rem 1.2rem;
-  box-shadow: var(--shadow-sm);
+  box-shadow: var(--shadow-md);
 `;
 
 export default Input;
