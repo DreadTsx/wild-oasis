f5867a86a3da75039704145a059a3339f97bef27
DreadTsx <hallowpahe@gmail.com>
2025-11-03 15:08:24 +0100

    made the Modal component a compound component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,23 +1,42 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
-
+import CabinTable from "./CabinTable";
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-  const handledisplayForm = () => {
-    setIsOpenModal((form) => !form);
-  };
   return (
-    <div>
-      <Button onClick={handledisplayForm}>Add New Cabin</Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add New Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      {/* <Modal.Open opens="table">
+        <Button>Show Table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window> */}
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+//   const handledisplayForm = () => {
+//     setIsOpenModal((form) => !form);
+//   };
+//   return (
+//     <div>
+//       <Button onClick={handledisplayForm}>Add New Cabin</Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
