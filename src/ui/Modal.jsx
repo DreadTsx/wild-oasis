7ee66205b563f8846caae44cdfe2283f69f9652f
DreadTsx <hallowpahe@gmail.com>
2025-11-06 21:48:39 +0100

    created a custom hook to detcect outside clicks(used this for the modal component to close the createcabin form)

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -2,6 +2,7 @@ import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -74,21 +75,21 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
-  if (name !== openName) {
-    return null;
-  } else {
-    return createPortal(
-      <Overlay>
-        <StyledModal>
-          <Button onClick={close}>
-            <HiXMark />
-          </Button>
-          <div>{cloneElement(children, { onCloseModal: close })}</div>
-        </StyledModal>
-      </Overlay>,
-      document.body
-    );
-  }
+  const { ref } = useOutsideClick(close);
+
+  if (name !== openName) return null;
+
+  return createPortal(
+    <Overlay>
+      <StyledModal ref={ref}>
+        <Button onClick={close}>
+          <HiXMark />
+        </Button>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
+      </StyledModal>
+    </Overlay>,
+    document.body
+  );
 }
 
 Modal.Open = Open;
