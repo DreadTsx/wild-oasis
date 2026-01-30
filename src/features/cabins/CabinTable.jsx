e1a498297d1fca42d226b94c44cfe9e3080dfb12
DreadTsx <hallowpahe@gmail.com>
2026-01-30 17:40:07 +0100

    feat: implemented the sorting features

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -35,7 +35,8 @@ function CabinTable() {
   const [searchParams] = useSearchParams();
   //
   if (isLoading) return <Spinner />;
-  //
+
+  //? 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabin;
@@ -44,6 +45,14 @@ function CabinTable() {
     filteredCabin = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filteredCabin = cabins.filter((cabin) => cabin.discount > 0);
+
+  //? 2) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabin.sort(
+    (a, b) => (a[field] - b[field]) * modifier,
+  );
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -57,7 +66,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabin}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
