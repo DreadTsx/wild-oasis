860a1e2150238f3685579cf2e3d47d907017574a
DreadTsx <hallowpahe@gmail.com>
2026-03-04 20:12:08 +0100

    feat: created the booking page (for each bookings)

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import Booking from "./pages/Booking";
 import AppLayout from "./ui/AppLayout";
 import { Toaster } from "react-hot-toast";
 
@@ -32,7 +33,8 @@ function App() {
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
-            <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings" element={<Bookings />} />{" "}
+            <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
